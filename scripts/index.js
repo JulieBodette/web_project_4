b934f97f3c713a9cbe297672f4697f89aa894443
JulieBodette <jr3python@gmail.com>
2022-05-04 17:30:30 -0400

    refactor: deleted duplicate code from popup class. image popup now has popup AND modal classes.

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,132 @@ const titleText = document.querySelector(".profile__info-title");
 const nameInput = formElement.querySelector('[name="name"]');
 const titleInput = formElement.querySelector('[name="title"]');
 
+<<<<<<< Updated upstream
+=======
+/////////////////////////////////////////////////////Set up add card button and modal for it
+const addCardButton = document.querySelector("#profile-add-button"); ///find the + button (add card)-this opens the modal panel
+const addCardModal = document.querySelector("#add-card-modal"); //using ID to find the modal (pop up).
+const addCardCloseButton = addCardModal.querySelector('.modal__close-button');//use the modal panel to find the stuff inside it.
+const addCardForm = addCardModal.querySelector(".modal__form"); //find the form.
+
+// find the form fields in the DOM
+const imageNameInput = addCardForm.querySelector('[name="imagename"]');
+const imageLinkInput = addCardForm.querySelector('[name="imagelink"]');
+/////////////////////////////////////////////
+
+//making the initial cards via Javascript
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ];
+//select the template, use .content to get the content inside the template, then query selector again to get the element class
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".element");
+//get a reference to the grid/container that we will put the cards in
+const cardsGrid = document.querySelector(".grid");
+
+//select the image popup and its picture and text and X (close button)
+const imagePopup = document.querySelector("#image-popup");
+const imagePopupPic = imagePopup.querySelector(".popup__image");
+const imagePopupText = imagePopup.querySelector(".popup__caption");
+const imagePopupCloseButton = imagePopup.querySelector(".modal__close-button");
+
+
+//create a card and add it to the cardsGrid
+//this function must be created before it is called because its stored in a variable
+const addCardElement = data => {
+    //data = name and link
+    //get the name and the link out of data (data is an object)
+    let cardname = data.name;
+    let cardlink = data.link;
+
+    //make a copy of the template using cloneNode
+    let newCard = cardTemplate.cloneNode(true); //true clones everything inside
+
+    //look within the card template for the spots where the name and link go, set them up
+    let Cardimage = newCard.querySelector(".element__image"); //used later to set up click event for image modal
+    Cardimage.style = "background-image:url('"+cardlink+"');";
+    //use .src here if image tag, I am using style and background image because it is button
+    newCard.querySelector(".element__text").textContent = cardname;
+
+    //query selector the like and delete button
+    let likebutton = newCard.querySelector(".element__like");
+    let deletebutton = newCard.querySelector(".element__trash");
+  
+    //add event listener for Cardimage- so that image modal pops up when clicked
+    Cardimage.addEventListener("click", function(evt){
+      console.log("wow and image");
+      createImagePopup(data);
+    });
+
+
+    //add event listeners for like and delete
+    likebutton.addEventListener("click", function(evt){
+      let Heart = evt.target;//the event target is the heart button that the user clicked on
+      Heart.classList.toggle("element__like_active");
+    });
+    deletebutton.addEventListener("click", function(evt){
+      let trash = evt.target;//the event target is the trash button that the user clicked on
+      let card = trash.parentElement.parentElement;//get the parent of trash. first parent is button, second is element div
+      card.remove();
+    });
+
+    //add the new card to the grid 
+    cardsGrid.append(newCard); 
+}
+
+
+//loop thru the initialCards array and send each one into the getCardElement function
+initialCards.forEach(
+    function (item){
+        addCardElement(item);         //get the card element and append it to the grid
+    }
+);
+
+////////////////////////////////////////////////////////////Set up image popup
+function createImagePopup(data) { //called in AddCardElement
+  //data = name and link
+  //get the name and the link out of data (data is an object)
+  //data.name data.link;
+
+  imagePopup.classList.add("modal_open");
+  imagePopupPic.src = data.link;
+  imagePopupText.textContent = data.name;
+  console.log("wow look an image popped up");
+  console.log(data);
+  //add event listener for closing the image popup
+}
+function CloseImagePopup() {
+  imagePopup.classList.remove("modal_open"); /*deactivate a class that makes it visible*/
+  }
+imagePopup.addEventListener("click", CloseImagePopup);
+
+
+/////////////////////////////////////////
+
+////////////////////////////////////////////////Set up edit profile modal
+>>>>>>> Stashed changes
 /////////////open and close the modal
 function OpenModal() {
 editProfileModal.classList.add("modal_open"); /*activate a class that makes it visible*/
