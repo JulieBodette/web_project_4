9d6791911d92c6742238819200afa27e68e07a2e
JulieBodette <jr3python@gmail.com>
2022-06-05 15:47:34 -0400

    Modified openImagePopup so it is not delegated

--- scripts/Card.js
+++ scripts/Card.js
@@ -8,17 +8,12 @@ class Card {
       //select the template, use .content to get the content inside the template, then query selector again to get the element class
       this.newCard; //will be set to the card element
       this.cardImage; //will be set to the image in the card
+
     }
     createCardElement()
     {
       //make a copy of the template using cloneNode
     this.newCard = this.cardTemplate.cloneNode(true); //true clones everything inside
-  
-    ///////////////////////////Use setattribute to store data in the object in the DOM
-    //now we can get to this data later, outside of the addCard function
-    this.newCard.setAttribute("data-name", this.cardName); //send setAtrribute parameters: name,value
-    //use setAtribute() because it is custom attribute. Also name must be in quotes
-    this.newCard.setAttribute("data-link", this.cardLink); //convention is that name is data-something
   
     this._setImageAndName();
     this._setEventListener();
@@ -28,20 +23,6 @@ class Card {
     }
     _setEventListener()
     {
-      //Delegated so there is only 1 listener per card which listens to clicks on image, like, and delete button
-      this.newCard.addEventListener("click", function(evt){
-        //class attributes and methods cannot be acessed from inside this function
-      if (evt.target.classList.contains("element__image")) {
-        const card = evt.target.closest(".element"); //get the card parent- this is where we stored the attributes
-        //get the attributes from the card. we stored the image name and image link as strings in the attributes.
-        const name = card.getAttribute("data-name");
-        const link = card.getAttribute("data-link");
-        setDataImagePopup(name, link);
-  
-        openModal(imagePopup);
-      }
-  
-    });
   
     //query selector the like and delete button
     const likeButton = this.newCard.querySelector(".element__like");
@@ -50,18 +31,24 @@ class Card {
     likeButton.addEventListener("click", this._like); //send it the function name ie this._like with NOPARENTHESES. this._like() BAD, WILL CALL FUNCTION ON PAGE LOAD
     deleteButton.addEventListener("click", this._delete);
 
+    //query selector the image. when this image is clicked on, a popup opens.
+    const cardImage = this.newCard.querySelector(".element__image");
+    //add event listener to image
+    cardImage.addEventListener("click", () => {this._openImagePopup()});
+    
 
     } //end _setEventListener
 
     _like(evt)
     {
-      console.log("liked");
       const heart = evt.target;//the event target is the heart button that the user clicked on
       heart.classList.toggle("element__like_active");
     }
 
-    _openImagePopup(evt)
+    _openImagePopup()
     {
+      setDataImagePopup(this.cardName, this.cardLink); 
+      openModal(imagePopup);
     }
 
     _delete(evt)
