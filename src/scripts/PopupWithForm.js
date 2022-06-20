71eee1e85f01dbd14dec5229eaf9f39fdef12365
JulieBodette <jr3python@gmail.com>
2022-06-19 20:35:46 -0400

    Added some code to PopupWithForm

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,6 +1,15 @@
 import Popup from "./popup"
 
 class PopupWithForm extends Popup{
+    constructor(popupSelector, submitFunction) //"#image-popup" is the popupSelector
+    {
+        super(popupSelector);
+    }
+
+    _getInputValues()
+    {
+        console.log("input values set");
+    }
 
 }
 
