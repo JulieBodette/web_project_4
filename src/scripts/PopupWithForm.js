9549aa0cfa31a8c34c31850ecfd9c9ac974acf03
JulieBodette <jr3python@gmail.com>
2022-06-19 22:21:18 -0400

    Worked on PopupWithForm

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -8,7 +8,21 @@ class PopupWithForm extends Popup{
 
     _getInputValues()
     {
+        //collects data from all the input fields and returns that data as an object.
         console.log("input values set");
+    }
+
+    setEventListeners()
+    {
+        
+        //add the click event listener to the close icon.
+        super.setEventListeners();
+        //add the submit event handler to the form 
+    }
+
+    close()
+    {
+        //reset the form once the popup is closed.
     }
 
 }
