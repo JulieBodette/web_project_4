5cdabb01725d90fd2983ae23dbe9c811cc682ee8
JulieBodette <jr3python@gmail.com>
2022-07-21 22:29:37 -0400

    made variables private in UserInfo class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,35 +3,35 @@ class UserInfo {
     { userName, userJob, userAvatar } //userName "profile__info-name" //userJob ".profile__info-title" //userAvatar ".profile__avatar"
   ) {
     //class names are sent in the constructor, we use the classes to find the objects in the DOM
-    this.userNameElement = document.querySelector(userName);
-    this.userJobElement = document.querySelector(userJob);
-    this.userAvatarElement = document.querySelector(userAvatar);
+    this._userNameElement = document.querySelector(userName);
+    this._userJobElement = document.querySelector(userJob);
+    this._userAvatarElement = document.querySelector(userAvatar);
   }
 
   setAvatar(avatar) {
-    this.userAvatarElement.src = avatar;
+    this._userAvatarElement.src = avatar;
   }
 
   setId(id) {
-    this.id = id; //being set here and not in the constructor
+    this._id = id; //being set here and not in the constructor
   }
 
   getId() {
-    return this.id;
+    return this._id;
   }
 
   setUserInfoTextOnly({ name, about }) {
-    this.userNameElement.textContent = name;
-    this.userJobElement.textContent = about;
+    this._userNameElement.textContent = name;
+    this._userJobElement.textContent = about;
   }
 
   getUserInfo() {
     //might need to add profile pic??
     // handy for cases when it's necessary to display the user data in the open form
     const newObj = {
-      name: this.userNameElement.textContent,
-      about: this.userJobElement.textContent,
-      id: this.id,
+      name: this._userNameElement.textContent,
+      about: this._userJobElement.textContent,
+      id: this.getId(),
     };
     return newObj;
   }
